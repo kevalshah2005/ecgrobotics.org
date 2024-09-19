f516f3a45f2bd0c5497e76788b88b533ca362016
Keval Shah <kevals2005@gmail.com>
2024-09-19 17:10:28 -0500

    Added hover effect to all images globally

--- src/components/pages/about/description/AboutDescription.jsx
+++ src/components/pages/about/description/AboutDescription.jsx
@@ -36,10 +36,8 @@ function AboutDescription() {
           
           <h2>What is FIRST?</h2>
           <p>FIRST (For Inspiration and Recognition of Science and Technology​) is a global non-profit organization that promotes science, technology, engineering, and math education through youth-oriented robotics competitions.</p>
-          <p>ECG Robotics fields teams that compete in two FIRST programs:</p>
-          <p><b>FIRST Tech Challenge (FTC):</b> FTC students work together with their mentors to design and build robots to compete in a dynamic and exciting challenge released every September. Teams program classroom-scale robots to follow autonomous commands before student drivers take control in two-on-two matches. On and off the field, students develop STEM skills, engage in community outreach, practice engineering innovation, and build confidence to help them succeed.</p>
-          <p><strong>FIRST Robotics Competition (FRC):</strong> Each January brings a new challenge for FRC teams. Starting with a Kit of Parts, they design, program, and build industrial-sized robots to play an action-packed game on a themed field as part of a three-team alliance. Guided by mentors and sponsors, students learn engineering and problem-solving skills as they compete for awards, create a team identity, raise funds, hone teamwork skills, and advance appreciation for STEM in their community.</p>
-        
+          <p>ECG Robotics fields teams that compete in two FIRST programs: FIRST Tech Challenge (FTC) and FIRST Robotics Competition (FRC)</p>
+                  
           <h2>Our History</h2>
           
         </div>
