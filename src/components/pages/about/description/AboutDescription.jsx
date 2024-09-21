0fc22c343630ed69f8a74c92257b5e6e6556bdf2
Keval Shah <kevals2005@gmail.com>
2024-09-21 00:11:48 -0500

    Added header image to About page

--- src/components/pages/about/description/AboutDescription.jsx
+++ src/components/pages/about/description/AboutDescription.jsx
@@ -19,26 +19,27 @@ function AboutDescription() {
   return (
     <div>
       <div className="about-us-container">
-        <div className="about-us-description-container">
+        <div className="about-us-header-container">
           <h1>About ECG Robotics</h1>
-          <h3>More about the club and its history</h3>
-
+          <h2>More about the club and its history</h2>
+        </div>
+        <div className="about-us-description-container">
           <Gallery 
-            images={galleryImages}
+            images={galleryImagesLeft}
             left={true}
           />
           <Gallery 
-            images={galleryImages}
+            images={galleryImagesRight}
             left={false}
           />
 
           <p>ECG Robotics is a student-led club in Greensboro, NC made up of five FIRST Robotics teams, working together each season to not only build robots, but also share our love of robotics and STEM with kids and adults in our community.</p>
           
-          <h2>What is FIRST?</h2>
+          <h1>What is FIRST?</h1>
           <p>FIRST (For Inspiration and Recognition of Science and Technology​) is a global non-profit organization that promotes science, technology, engineering, and math education through youth-oriented robotics competitions.</p>
           <p>ECG Robotics fields teams that compete in two FIRST programs: FIRST Tech Challenge (FTC) and FIRST Robotics Competition (FRC).</p>
                   
-          <h2>Our History</h2>
+          <h1>Our History</h1>
         </div>
       </div>
     </div>
