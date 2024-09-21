7dae7e7cfd9165145f9b9c32fde8fc969aa1587c
Keval Shah <kevals2005@gmail.com>
2024-09-21 16:25:40 -0500

    Moved cards css to CardItem.css file

--- src/components/card/CardItem.jsx
+++ src/components/card/CardItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import '../pages/home/description/Description.css'
+import './CardItem.css'
 
 function CardItem(props) {
   return (
