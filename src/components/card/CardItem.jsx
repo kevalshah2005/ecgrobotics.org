641f04a4c93d24eac6df8b54a413e42a9f0fe0c6
Keval Shah <kevals2005@gmail.com>
2024-10-04 00:12:08 -0500

    Added basic cards to Sponsor page

--- src/components/card/CardItem.jsx
+++ src/components/card/CardItem.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './CardItem.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './CardItem.css';
 
 function CardItem(props) {
-  return (
+  const content = (
     <>
-        <div className="cards-item">
-            <Link className="cards-item-link" to={props.path} style = {{ backgroundColor: props.backgroundColor }}>
-                <figure className="cards-item-pic-wrap" >
-                    <img src={props.src} alt='Team Image' className="cards-item-img" />
-                </figure>
-                <div className="cards-item-info">
-                    <h5 className="cards-item-title">{props.title}</h5>
-                    <h5 className='cards-item-number'>{props.number}</h5>
-                    <p className='cards-item-description'>{props.description}</p>
-                </div>
-            </Link>
-        </div>
+      <figure className="cards-item-pic-wrap">
+        <img src={props.src} alt="Team Image" className="cards-item-img" />
+      </figure>
+      <div className="cards-item-info">
+        <h5 className="cards-item-title">{props.title}</h5>
+        <h5 className="cards-item-number">{props.number}</h5>
+        <p className="cards-item-description">{props.description}</p>
+      </div>
     </>
-  )
+  );
+
+  return (
+    <div className="cards-item">
+      {props.path ? (
+        <Link className="cards-item-link" to={props.path} style={{ backgroundColor: props.backgroundColor }}>
+          {content}
+        </Link>
+      ) : (
+        <div className="cards-item-no-link" style={{ backgroundColor: props.backgroundColor }}>
+          {content}
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default CardItem
+export default CardItem;
--- src/components/pages/sponsors/description/SponsorsDescription.jsx
+++ src/components/pages/sponsors/description/SponsorsDescription.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import './SponsorsDescription.css'
 import sponsorImage from '../../../../assets/sponsorImage.webp'
+import CardItem from '../../../card/CardItem'
+import logo731 from '../../../../assets/logo731.webp'
+import logo5795 from '../../../../assets/logo5795.webp'
+import logo6183 from '../../../../assets/logo6183.webp'
+import logo10195 from '../../../../assets/logo10195.webp'
+import logo1533 from '../../../../assets/logo1533.webp'
 import Gallery3D from '../../../gallery3d/Gallery3D'
 
 function SponsorsDescription() {
@@ -23,7 +29,59 @@ function SponsorsDescription() {
             <p>Ready? Contact us at <a href="mailto: info@ecgrobotics.org">info@ecgrobotics.org</a> and ask how you can help build tomorrow’s engineers, programmers, designers, and thinkers.</p>
             <img src={sponsorImage} alt="Image showing robot with sponsor logos on it" />
         </div>
-        <Gallery3D />
+
+        {/* <Gallery3D /> */}
+
+        <div className='cards'>
+          <h1>Sponsorship Tiers</h1>
+          <div className="cards-container">
+            <div className="cards-wrapper">
+              <ul className="cards-items">
+                <CardItem 
+                  src={logo731}
+                  title='Diamond'
+                  number='$7500+ Annual Contribution'
+                  description="Wannabee Strange is a 12-year team where members learn to apply real-world skills. We've received the NC Inspire award in the last 2 years, gone to Worlds for 4 years running, and in 2019 we received a robot design nomination. 731 gives back with our Varsity Letter Initiative, pushing for the recognition of robotics as a varsity sport worldwide."
+                />
+                <CardItem 
+                  src={logo5795}
+                  title='Platinum'
+                  number='$5000-7499 Annual Contribution'
+                  description="Back to the Drawing Board has 6 years of experience and we have been to the World Championships 3 times! Every day, our team strives to build better robots, help out our sister teams, and spread the core values of FIRST to the community—especially through STEM Education Events—no matter how many times we have to go back to the drawing board."
+                  path='/ftc5795'
+                />
+                <CardItem 
+                  src={logo6183}
+                  title='Gold'
+                  number='$3000-4999 Annual Contribution'
+                  description="The Thunder Ducks were established in 2014 and have consistently gone to the NC State Competition since. Our members are constantly helping other teams, teaching each other, and growing together as we strive together to continue to be better each and every day. We do this with our children's book series—Beek Beek Books—and by cultivating STEM skills for the real-world."
+                  path='/ftc6183'
+                />
+                <CardItem 
+                  src={logo10195}
+                  title='Silver'
+                  number='$1000-2499 Annual Contribution'
+                  description="The Night Owls formed in 2015. Every year enthusiastic individuals come to fill old members' shoes. Our team consists of a mixture of students from schools around Greensboro. We've expanded our love for our team by making an owl-themed mascot complete with hand-made wings along with our own owl robot, Otto! We hope to go to Worlds for the first time this year!"
+                  path='/ftc10195'
+                />
+                <CardItem 
+                  src={logo10195}
+                  title='Bronze'
+                  number='$500-999 Annual Contribution'
+                  description="The Night Owls formed in 2015. Every year enthusiastic individuals come to fill old members' shoes. Our team consists of a mixture of students from schools around Greensboro. We've expanded our love for our team by making an owl-themed mascot complete with hand-made wings along with our own owl robot, Otto! We hope to go to Worlds for the first time this year!"
+                  path='/ftc10195'
+                />
+                <CardItem 
+                  src={logo10195}
+                  title='Honorable Mention'
+                  number='$0-499 Annual Contribution'
+                  description="The Night Owls formed in 2015. Every year enthusiastic individuals come to fill old members' shoes. Our team consists of a mixture of students from schools around Greensboro. We've expanded our love for our team by making an owl-themed mascot complete with hand-made wings along with our own owl robot, Otto! We hope to go to Worlds for the first time this year!"
+                  path='/ftc10195'
+                />
+              </ul>
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
