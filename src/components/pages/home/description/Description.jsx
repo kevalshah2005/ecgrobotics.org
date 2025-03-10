ef06c43a9dc1cee5cbbe59322f3283610717c7b3
Keval Shah <kevals2005@gmail.com>
2025-03-09 23:38:00 -0500

    Removed description from FRC card and enforced minimum card width

--- src/components/pages/home/description/Description.jsx
+++ src/components/pages/home/description/Description.jsx
@@ -83,7 +83,6 @@ function Description() {
                   src={logo1533}
                   title='Triple Strange'
                   number='1533'
-                  description="Triple Strange is a 3-time state champion team, established in 2004. We were the first team to be a part of ECG robotics. We typically accept those from grades 9-12, but we also enjoy having younger groups shadowing us and learning as we often encourage them to join the team as well. We keep striving to be unusually good at the impossible as we grow every day."
                   path='/frc1533'
                   glowColor="var(--blue)"
                 />
