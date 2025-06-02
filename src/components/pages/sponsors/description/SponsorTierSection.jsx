b263511f84551f4c1d68c05e3fa71ca3460c2dbf
Keval Shah <kevals2005@gmail.com>
2025-06-01 21:23:05 -0400

    Converted sponsors to cards

---
+++ src/components/pages/sponsors/description/SponsorTierSection.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import CardItem from '../../../card/CardItem';
+
+function SponsorTierSection({ title, description, sponsors, fallback }) {
+  return (
+    <>
+      <h2 data-aos="fade-up">{title}</h2>
+      <p data-aos="fade-up">{description}</p>
+      {
+        sponsors.length > 0 ? (
+          <div className='cards' data-aos="fade-up">
+            <div className="cards-container">
+              <div className="cards-wrapper">
+                <ul className="cards-items">
+                  {sponsors.map((sponsor, index) => (
+                    <CardItem
+                      key={sponsor.name + index}
+                      title={sponsor.name}
+                      src={sponsor.logo}
+                      path={sponsor.link}
+                    />
+                  ))}
+                </ul>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <p data-aos="fade-up">{fallback}</p>
+        )
+      }
+    </>
+  );
+}
+
+export default SponsorTierSection;
