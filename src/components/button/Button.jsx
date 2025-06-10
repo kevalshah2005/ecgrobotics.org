b1752ca94ca331d58a4edc32c4aa86760b70bf78
Keval Shah <kevals2005@gmail.com>
2025-06-10 17:13:26 -0400

    Removed unused hoverColor prop

--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 const STYLES = ['btn--primary', 'btn--outline'];
 const SIZES = ['btn--medium', 'btn--large'];
 
-export const Button = ({children, type, onClick, buttonStyle, buttonSize, location, hoverColor}) => {
+export const Button = ({children, type, onClick, buttonStyle, buttonSize, location}) => {
     const checkButtonStyle = STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
 
