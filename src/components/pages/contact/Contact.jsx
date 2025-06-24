a63a6afb45730f6dd4b2dc30ff01fb3422564a09
Keval Shah <kevals2005@gmail.com>
2025-06-23 23:39:19 -0400

    Made contact page better on mobile and made headers underlined

--- src/components/pages/contact/Contact.jsx
+++ src/components/pages/contact/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => (
                 Greensboro, NC 27410
             </p>
             <p>
-                <b>Team Contacts:</b><br />
+                <b className="contacts-header"><u>Team Contacts</u></b><br />
                 <b>FRC Team 1533:</b> <a href="mailto:frc1533@ecgrobotics.org">frc1533@ecgrobotics.org</a><br />
                 <b>FTC Team 731:</b> <a href="mailto:ftc731@ecgrobotics.org">ftc731@ecgrobotics.org</a><br />
                 <b>FTC Team 5795:</b> <a href="mailto:ftc5795@ecgrobotics.org">ftc5795@ecgrobotics.org</a><br />
@@ -29,7 +29,7 @@ const Contact = () => (
                 <b>FTC Team 10195:</b> <a href="mailto:ftc10195@ecgrobotics.org">ftc10195@ecgrobotics.org</a>
             </p>
             <p>
-                <b>Leadership Contacts:</b><br />
+                <b className="contacts-header"><u>Leadership Contacts</u></b><br />
                 <b>Parent President:</b> <a href="mailto:boardpresident@ecgrobotics.org">boardpresident@ecgrobotics.org</a><br />
                 <b>Student President:</b> <a href="mailto:studentpresident@ecgrobotics.org">studentpresident@ecgrobotics.org</a><br />
             </p>
