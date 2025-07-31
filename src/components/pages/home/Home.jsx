9cc331f9be1eb4f0e485f537a072e863f9465005
Keval Shah <kevals2005@gmail.com>
2025-07-30 23:45:45 -0400

    Added favicon

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import '../../../App.css'
 import Landing from '../home/landing/Landing'
 import Description from './description/Description';
+import Favicon from '/assets/logo1533.ico'
 
 function Home() {
     return (
         <>
+            <link rel="icon" href={Favicon}></link> /* Set the favicon path */
             <Landing />
             <Description />
         </>
