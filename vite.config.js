81bdd4d6b31c73126711ccab273a9648f28a8ff5
Keval Shah <kevals2005@gmail.com>
2025-08-02 17:17:00 -0400

    Revert "Added sitemap generator and removed dist from sitemap"
    This reverts commit 7dd0edaa858ff55dd1b7bf74a7b8a109b76e78a5.

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import Sitemap from 'vite-plugin-sitemap'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), Sitemap({
-    hostname: 'https://ecgrobotics.org'
-  })],
+  plugins: [react()],
   base: "/"
 })
