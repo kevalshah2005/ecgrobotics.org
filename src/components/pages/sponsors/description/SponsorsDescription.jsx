334034af010257d9f3248f709d4ebe6afbce927a
Keval Shah <kevals2005@gmail.com>
2025-08-09 23:39:38 -0400

    Updated tiers/descriptions and made initial Strange tier styling

--- src/components/pages/sponsors/description/SponsorsDescription.jsx
+++ src/components/pages/sponsors/description/SponsorsDescription.jsx
@@ -16,6 +16,12 @@ import QorvoLogo from '/assets/SponsorLogos/QorvoLogo.webp';
 
 const sponsors = [
   {
+    name: 'ECG Robotics',
+    level: 'strange',
+    logo: 'https://ecgrobotics.org/assets/logo.webp',
+    link: 'https://ecgrobotics.org/',
+  },
+  {
     name: 'Qorvo',
     level: 'diamond',
     logo: QorvoLogo,
@@ -81,6 +87,13 @@ const sponsors = [
     logo: UnitedWayLogo,
     link: 'https://www.unitedway.org',
   },
+  // Example Strange sponsor
+  // {
+  //   name: 'Example Strange Sponsor',
+  //   level: 'strange',
+  //   logo: ExampleLogo,
+  //   link: 'https://example.com',
+  // },
 ];
 
 const imageWidth = '80%';
@@ -90,12 +103,13 @@ const getSponsorsByLevel = (level) => {
 };
 
 function SponsorsDescription() {
+  const strangeSponsors = getSponsorsByLevel('strange');
   const diamondSponsors = getSponsorsByLevel('diamond');
   const platinumSponsors = getSponsorsByLevel('platinum');
   const goldSponsors = getSponsorsByLevel('gold');
   const silverSponsors = getSponsorsByLevel('silver');
   const bronzeSponsors = getSponsorsByLevel('bronze');
-  const honorableMentionSponsors = getSponsorsByLevel('honorable mention')
+  const honorableMentionSponsors = getSponsorsByLevel('honorable mention');
 
   const cardWidth = '300px';
 
@@ -109,75 +123,82 @@ function SponsorsDescription() {
         </div>
       </div>
       <div className="sponsors-description-container">
-        <p>Thanks to our generous sponsors, our teams have inspired others while taking home state championship titles and earning places at the FIRST World Championships in Houston.
+        <p>
+          Thanks to our generous sponsors, our teams have inspired others while taking home state championship titles and earning places at the FIRST World Championships in Houston.
           <b> Your company’s donation helps fund materials, tools, workspaces, and competition expenses </b>
           that will provide students with hands-on experience building not just robots, but
           <b> real-world skills </b>
           that will stay with them into college and beyond.
         </p>
-        <p>Ready? Contact us at <a href="mailto: info@ecgrobotics.org" style={{ fontSize: "inherit" }}>info@ecgrobotics.org</a> and ask how you can help build tomorrow’s engineers, programmers, designers, and thinkers.</p>
+        <p>
+          Ready? Contact us at <a href="mailto: info@ecgrobotics.org" style={{ fontSize: "inherit" }}>info@ecgrobotics.org</a> and ask how you can help build tomorrow’s engineers, programmers, designers, and thinkers.
+        </p>
         <img src={sponsorImage} alt="Image showing robot with sponsor logos on it" />
-      </div>
 
-      <div className="sponsorship-tiers">
-        <h1 data-aos="fade-up">Our Sponsors</h1>
-        <SponsorTierSection
-          title="Diamond - $7500+ Annual Contribution"
-          description="Description of Diamond level"
-          sponsors={diamondSponsors}
-          fallback="No diamond sponsors yet."
-          width={cardWidth}
-          imageWidth={imageWidth}
-        />
+        <div className="sponsorship-tiers">
+          <h1 data-aos="fade-up">Our Sponsors</h1>
 
-        <SponsorTierSection
-          title="Platinum - $5000-$7499 Annual Contribution"
-          description="Description of Platinum level"
-          sponsors={platinumSponsors}
-          fallback="No platinum sponsors yet."
-          width={cardWidth}
-          imageWidth={imageWidth}
-        />
+          <div className="strange-tier">
+            <div className="strange-tier-inner">
+              <SponsorTierSection
+                title="Strange - $10,000+ Annual Contribution"
+                description="Company name listed as Title Sponsor of ECG Robotics. Receive 10 additional organization shirts. Prominent logo placement across all robots, shirts, banners, and digital materials. All Diamond level benefits."
+                sponsors={strangeSponsors}
+                fallback="No Strange level sponsors yet."
+                width={cardWidth}
+                imageWidth={imageWidth}
+              />
+            </div>
+          </div>
 
-        <SponsorTierSection
-          title="Gold - $2500-$4999 Annual Contribution"
-          description="Description of Gold level"
-          sponsors={goldSponsors}
-          fallback="No gold sponsors yet."
-          width={cardWidth}
-          imageWidth={imageWidth}
-        />
+          <SponsorTierSection
+            title="Diamond - $7,500-$9,999 Annual Contribution"
+            description="FTC and FRC demonstration at your workplace (within reasonable travel distance, upon request). Company name and logo listed on all social media profiles and outreach materials. Receive 5 additional organization shirts. Large logo on FTC & FRC robots, t-shirts, trifolds, and banners. All Platinum level benefits."
+            sponsors={diamondSponsors}
+            fallback="No diamond sponsors yet."
+            width={cardWidth}
+            imageWidth={imageWidth}
+          />
 
-        <SponsorTierSection
-          title="Silver - $1000-$2499 Annual Contribution"
-          description="Description of Silver level"
-          sponsors={silverSponsors}
-          fallback="No silver sponsors yet."
-          width={cardWidth}
-          imageWidth={imageWidth}
-        />
+          <SponsorTierSection
+            title="Platinum - $5,000-$7,499 Annual Contribution"
+            description="Company promotion opportunity at full-team FRC meeting and full-teams FTC meeting upon request. Receive 2 organization shirts. Tour of our workspace for your employees or guests (upon request). All Gold level benefits."
+            sponsors={platinumSponsors}
+            fallback="No platinum sponsors yet."
+            width={cardWidth}
+            imageWidth={imageWidth}
+          />
 
-        <SponsorTierSection
-          title="Bronze - $500-$999 Annual Contribution"
-          description="Description of Bronze level"
-          sponsors={bronzeSponsors}
-          fallback="No bronze sponsors yet."
-          width={cardWidth}
-          imageWidth={imageWidth}
-        />
+          <SponsorTierSection
+            title="Gold - $2,500-$4,999 Annual Contribution"
+            description="Small logo on FTC and FRC robots. Small logo on pamphlets, trifolds, banners and t-shirts. Invite to annual end-of-season banquet. All Silver level benefits."
+            sponsors={goldSponsors}
+            fallback="No gold sponsors yet."
+            width={cardWidth}
+            imageWidth={imageWidth}
+          />
 
-        <SponsorTierSection
-          title="Honorable Mention - $100-$499 Annual Contribution"
-          description="Description of Honorable Mention level"
-          sponsors={honorableMentionSponsors}
-          fallback="No honorable mention sponsors yet."
-          width={cardWidth}
-          imageWidth={imageWidth}
-        />
+          <SponsorTierSection
+            title="Silver - $1,000-$2,499 Annual Contribution"
+            description="Invitation to FTC and FRC outreach events. Custom sponsor social media post. Name/logo included in newsletter. All Bronze level benefits."
+            sponsors={silverSponsors}
+            fallback="No silver sponsors yet."
+            width={cardWidth}
+            imageWidth={imageWidth}
+          />
 
+          <SponsorTierSection
+            title="Bronze - $100-$999 Annual Contribution"
+            description="Company logo and link on sponsors page of website. Subscription to ECG Robotics’ newsletter, featuring seasonal updates and highlights. Personalized thank you letter. Invitation to attend local FTC and FRC competitions."
+            sponsors={bronzeSponsors}
+            fallback="No bronze sponsors yet."
+            width={cardWidth}
+            imageWidth={imageWidth}
+          />
+        </div>
       </div>
     </div>
   )
 }
 
-export default SponsorsDescription
+export default SponsorsDescription;
