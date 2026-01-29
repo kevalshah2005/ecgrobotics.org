36748b4d93eee0b96abcdf990df87dceb89c1c1d
Keval Shah <kevals2005@gmail.com>
2026-01-29 17:11:12 -0600

    separated ftc and frc description sections

--- src/components/pages/home/description/Description.jsx
+++ src/components/pages/home/description/Description.jsx
@@ -9,6 +9,8 @@ import logo5795 from '../../../../assets/logo5795.webp'
 import logo6183 from '../../../../assets/logo6183.webp'
 import logo10195 from '../../../../assets/logo10195.webp'
 import logo1533 from '../../../../assets/logo1533.webp'
+import landingVideoThumbnail from '../../../../assets/LandingVideoThumbnail.webp'
+import frcSectionBackground from '../../../../assets/FRCSectionBackground.png'
 import "aos/dist/aos.css";
 
 function Description() {
@@ -22,72 +24,74 @@ function Description() {
         <img src={overviewImage} alt='' className='overview-image'/>
       </div>
 
-      <div className='ftc-container'>
-        <h1 data-aos="fade-up">FIRST Tech Challenge</h1>
-        <div className='ftc-description-container' data-aos="fade-up">
-          <p>FIRST Tech Challenge (FTC) is a robotics competition for middle and high school students, where teams design, build, and program robots to complete an annual challenge. Using engineering, coding, and teamwork, students develop problem-solving skills while applying real-world STEM concepts. FTC also encourages outreach, with teams mentoring younger students and promoting STEM in their communities.
-          </p>
-          <img src={ftcImage} alt='' className='ftc-image' />
-        </div>
-        <div className='cards' data-aos="fade-up">
-          <h1>Check out our teams here!</h1>
-          <div className="cards-container">
-            <div className="cards-wrapper">
-              <ul className="cards-items">
-                <CardItem 
-                  src={logo731}
-                  title='Wannabee Strange'
-                  number='731'
-                  path='/ftc731'
-                  glowColor="var(--orange)"
-                />
-                <CardItem 
-                  src={logo5795}
-                  title='Back to the Drawing Board'
-                  number='5795'
-                  path='/ftc5795'
-                  glowColor="var(--orange)"
-                />
-                <CardItem 
-                  src={logo6183}
-                  title='Thunderducks'
-                  number='6183'
-                  path='/ftc6183'
-                  glowColor="var(--orange)"
-                />
-                <CardItem 
-                  src={logo10195}
-                  title='Night Owls'
-                  number='10195'
-                  path='/ftc10195'
-                  glowColor="var(--orange)"
-                />
-              </ul>
+      <div className="section-box ftc-box" data-aos="fade-up">
+        <div className='ftc-container' style={{ backgroundImage: `url(${landingVideoThumbnail})` }}>
+          <h1>FIRST Tech Challenge</h1>
+          <div className='ftc-description-container'>
+            <p>FIRST Tech Challenge (FTC) is a robotics competition for middle and high school students, where teams design, build, and program robots to complete an annual challenge. Using engineering, coding, and teamwork, students develop problem-solving skills while applying real-world STEM concepts. FTC also encourages outreach, with teams mentoring younger students and promoting STEM in their communities.</p>
+            <img src={ftcImage} alt='' className='ftc-image' />
+          </div>
+          <div className='cards'>
+            <h1>Check out our teams here!</h1>
+            <div className="cards-container">
+              <div className="cards-wrapper">
+                <ul className="cards-items">
+                  <CardItem 
+                    src={logo731}
+                    title='Wannabee Strange'
+                    number='731'
+                    path='/ftc731'
+                    glowColor="var(--orange)"
+                  />
+                  <CardItem 
+                    src={logo5795}
+                    title='Back to the Drawing Board'
+                    number='5795'
+                    path='/ftc5795'
+                    glowColor="var(--orange)"
+                  />
+                  <CardItem 
+                    src={logo6183}
+                    title='Thunderducks'
+                    number='6183'
+                    path='/ftc6183'
+                    glowColor="var(--orange)"
+                  />
+                  <CardItem 
+                    src={logo10195}
+                    title='Night Owls'
+                    number='10195'
+                    path='/ftc10195'
+                    glowColor="var(--orange)"
+                  />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
 
-      <div className="frc-container">
-      <h1 data-aos="fade-up">FIRST Robotics Competition</h1>
-        <div className='frc-description-container' data-aos="fade-up">
-          <p> This is filler text. In this space will be an explanation about what FRC is. Ideally I'd like to keep it a general program overview rather than anything about the team, since that's already below.
-          </p>
-          <img src={frcImage} alt='' className='frc-image' />
-        </div>
-        <div className='cards' data-aos="fade-up">
-          <h1>Check out our team here!</h1>
-          <div className="cards-container">
-            <div className="cards-wrapper">
-              <ul className="cards-items">
-                <CardItem 
-                  src={logo1533}
-                  title='Triple Strange'
-                  number='1533'
-                  path='/frc1533'
-                  glowColor="var(--blue)"
-                />
-              </ul>
+      <div className="section-box frc-box" data-aos="fade-up">
+        <div className="frc-container" style={{ backgroundImage: `url(${frcSectionBackground})` }}>
+          <h1>FIRST Robotics Competition</h1>
+          <div className='frc-description-container'>
+            <p>This is filler text. In this space will be an explanation about what FRC is. Ideally I'd like to keep it a general program overview rather than anything about the team, since that's already below.</p>
+            <img src={frcImage} alt='' className='frc-image' />
+          </div>
+          <div className='cards'>
+            <h1>Check out our team here!</h1>
+            <div className="cards-container">
+              <div className="cards-wrapper">
+                <ul className="cards-items">
+                  <CardItem 
+                    src={logo1533}
+                    title='Triple Strange'
+                    number='1533'
+                    path='/frc1533'
+                    glowColor="var(--blue)"
+                  />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
